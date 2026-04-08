0080659deadaa4bc6a4613df3324292d52d77bbf
premprakash011-hue <prem.prakash011@gmail.com>
2026-04-08 12:05:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+# app.py
+
+from fastapi import FastAPI, File, UploadFile
+import torch
+import torch.nn as nn
+from torchvision import transforms
+from PIL import Image
+import io
+
+# CNN MODEL
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.fc_layers = nn.Sequential(
+            nn.Linear(64 * 7 * 7, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_layers(x)
+        return x
+
+
+# FASTAPI APP
+app = FastAPI(title="MNIST CNN API")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = CNN().to(device)
+model.load_state_dict(torch.load("mnist_cnn_model.pkl", map_location=device))
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.Resize((28, 28)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+@app.get("/")
+def home():
+    return {"message": "MNIST CNN API is running"}
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("L")
+
+    image = transform(image).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        outputs = model(image)
+        probabilities = torch.softmax(outputs, dim=1)
+        confidence, predicted = torch.max(probabilities, 1)
+
+    return {
+        "predicted_digit": int(predicted.item()),
+        "confidence": float(confidence.item())
+    }
